7ff6ba748f7d0102acc6410ffb7d4de6ab7c5b4a
Gavin Thompson <gavin.thompson2@googlemail.com>
2022-09-26 15:41:01 +0100

    Updated logging to capture URL and timestamp successfully tested with NLTK dep_fetcher

--- provisioning/roles/mitm-server/bootstrap/templates/log.py
+++ provisioning/roles/mitm-server/bootstrap/templates/log.py
@@ -6,16 +6,18 @@ import time
 def response(flow):
     url_request: str = str(flow.request.pretty_url)
     url_request = url_request.replace("/", "_")
+    time_stamp = str(int(time.time()))
     hash_string = (
     hashlib.sha1(url_request.encode()).hexdigest()[:10] +
         '_' +
-        str(int(time.time())) +
+        time_stamp +
         '.json'
     )
 
     with open(str('/home/vagrant/req/' + hash_string), "ab") as ofile:
         d = dict()
         d['url'] = str(flow.request.pretty_url.encode())
+        d['time_stamp'] = time_stamp
         d['headers'] = dict(flow.request.headers.items())
         d = json.dumps(d)
         ofile.write(d.encode())
